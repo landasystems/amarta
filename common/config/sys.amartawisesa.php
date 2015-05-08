0ca13dadd40e02857464e2a5c3d9eda263f528b6
yuliantofrandi <yuliantofrandi@laptop1>
2015-05-09 00:26:35 +0700

    Merge origin/master
    Conflicts:
    	.gitignore
    	nbproject/project.properties
    	nbproject/project.xml

--- common/config/sys.amartawisesa.php
+++ common/config/sys.amartawisesa.php
@@ -1,13 +1,13 @@
 <?php
 //=========change this scope=========================================================
-$root = '/var/www/html/asrul/amarta/';
-$themesUrl = 'http://localhost/asrul/amarta/common/themes/';
+$root = '/var/www/amarta/';
+$themesUrl = 'http://localhost/amarta/common/themes/';
 $db = 'landa_ims_amartawisesa';
 $dbUser = 'root';   
 $dbPwd = 'landak';
 $client = 'sys';
 $clientName = 'Manufacture & Payroll - Amarta Wisesa';
 $menu = array('manufacture');
-$rootUrl = 'http://localhost/asrul/amarta/amarta/www/sys/';
+$rootUrl = 'http://localhost/amarta/amarta/www/sys/';
 //===================================================================================
 ?>
