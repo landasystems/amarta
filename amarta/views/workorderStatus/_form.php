3f4917894ca79547f3b82bf38069129c13e2f9e3
asrulsibaoel <asrulsibaoel@asrulsibaoel-PC>
2015-05-09 09:31:38 +0700

    Merge origin/master
    Conflicts:
    	taro_simpeg/views/report/_pegawai.php
    	taro_simpeg/views/report/_pensiun.php

--- amarta/views/workorderStatus/_form.php
+++ amarta/views/workorderStatus/_form.php
@@ -137,15 +137,15 @@
                                                 <input class="angka" value="'.$value->Process->name.'" maxlength="60" prepend="Rp" type="text" id="charge" name="charge">
                                             </div>
                                         </td>
-                                        <td><input type="text" class="angka" name="start_amount[]"></td>
-                                        <td><input type="text" class="angka" name="end_amount[]"></td>
-                                        <td><input type="text" class="angka" name="lost[]"></td>
-                                        <td><input type="text" class="angka" name="lost_charge[]"></td>
+                                        <td><input type="text" class="angka" name="start_amount[]" value="'.$value->start_qty.'"></td>
+                                        <td><input type="text" class="angka" name="end_amount[]" value="'.$value->end_qty.'"></td>
+                                        <td><input type="text" class="angka" name="lost[]" value="'.$value->loss_qty.'"></td>
+                                        <td><input type="text" class="angka" name="lost_charge[]" value="'.$value->loss_charge.'"></td>
                                         <td>
                                             <a id="btnRemove" class="btn" href="#"><i class="cut-icon-minus-2"></i></a>
-                                            <input type="hidden" name="id[]" class="work_id" value="">
-                                            <input type="hidden" name="process_id[]" class="process_id" value="">
-                                            <input type="hidden" name="split_id[]" class="split_id" value="">
+                                            <input type="hidden" name="id[]" class="work_id" value="'.$value->id.'">
+                                            <input type="hidden" name="process_id[]" class="process_id" value="'.$value->work_process_id.'">
+                                            <input type="hidden" name="split_id[]" class="split_id" value="'.$value->workorder_split_id.'">
                                         </td>
                                     </tr>
                                 ';
